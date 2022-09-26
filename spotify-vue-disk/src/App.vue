6fabcef17382a3fce0d3a5c9426ee8b6f29d3a97
antoinde <antonello.indelicato98@gmail.com>
2022-09-26 20:29:44 +0200

    First commit

---
+++ spotify-vue-disk/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    
+  </div>
+</template>
+
+<script>
+//import component from 'path'
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style lang="scss">
+  // import variables
+  
+#app {
+  
+}
+</style>
